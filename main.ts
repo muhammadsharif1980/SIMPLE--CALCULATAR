5b37f32ae431673e54a2efe65e7b850dc17a98d4
Muhammad Sharif <160061606+muhammadsharif1980@users.noreply.github.com>
2024-03-25 03:38:42 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env Nodecls
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+const asnwer = await inquirer.prompt([
+    { message: "Enter frist nummber", tupe: "number", name: "firstNumber" },
+    { message: "Enter scecond number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//conditional statment
+if (asnwer.operator === "Addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+}
+else if (asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+}
+else if (asnwer.operator === "Multiplication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber);
+}
+else if (asnwer.operator === "Division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber);
+}
+else {
+    console.log("please select valid operator ");
+}
